62831e8381b7cf73b6f26048325b4de25315560c
harsh4277 <103769647+harsh4277@users.noreply.github.com>
2023-07-24 11:50:23 +0530

    Update recognize.py
    The usage main statement was commented due to which error was there so I changed it.

--- PyPower_face-recognition/recognize.py
+++ PyPower_face-recognition/recognize.py
@@ -1,6 +1,5 @@
 # USAGE
-
-#python recognize.py --detector face_detection_model --embedding-model openface_nn4.small2.v1.t7 --recognizer output/PyPower_recognizer.pickle --le output/PyPower_label.pickle --image images/test1.jpg
+python recognize.py --detector face_detection_model --embedding-model openface_nn4.small2.v1.t7 --recognizer output/PyPower_recognizer.pickle --le output/PyPower_label.pickle --image images/test1.jpg
 
 # import the necessary packages
 import numpy as np
